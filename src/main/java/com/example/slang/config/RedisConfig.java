f6e4a4a38b823a56449ed7b4ec09972f770e459e
airoca <jyh0872@gmail.com>
2024-05-28 00:16:52 +0900

    test: Rollback

--- src/main/java/com/example/slang/config/RedisConfig.java
+++ src/main/java/com/example/slang/config/RedisConfig.java
@@ -2,14 +2,12 @@ package com.example.slang.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@Profile("!test")
 public class RedisConfig {
 
     @Bean
@@ -22,10 +20,8 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
 
-        // Use StringRedisSerializer for keys
         template.setKeySerializer(new StringRedisSerializer());
 
-        // Use GenericJackson2JsonRedisSerializer for values
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return template;
