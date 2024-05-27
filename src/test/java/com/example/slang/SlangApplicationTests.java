2611c1f4270bb261f013a55332a232397cd9bcec
airoca <jyh0872@gmail.com>
2024-05-28 00:08:06 +0900

    test: Redis EC2 test3

--- src/test/java/com/example/slang/SlangApplicationTests.java
+++ src/test/java/com/example/slang/SlangApplicationTests.java
@@ -2,17 +2,13 @@ package com.example.slang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SlangApplicationTests {
-
-	@MockBean
-	private RedisTemplate<String, Object> redisTemplate;
 
 	@Test
 	void contextLoads() {
 	}
-
 }
